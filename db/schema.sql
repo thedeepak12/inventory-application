ccdb1fa1d88d95f06b1bfeaf00d5e2c5894023fe
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-09-05 19:16:12 +0530

    Modify schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-CREATE TABLE genres (
+CREATE TABLE IF NOT EXISTS genres (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE developers (
+CREATE TABLE IF NOT EXISTS developers (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE games (
+CREATE TABLE IF NOT EXISTS games (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  release_date DATE
+  image_url TEXT
 );
 
-CREATE TABLE game_genres (
+CREATE TABLE IF NOT EXISTS game_genres (
   game_id INTEGER REFERENCES games(id) ON DELETE CASCADE,
   genre_id INTEGER REFERENCES genres(id) ON DELETE CASCADE,
   PRIMARY KEY (game_id, genre_id)
 );
 
-CREATE TABLE game_developers (
+CREATE TABLE IF NOT EXISTS game_developers (
   game_id INTEGER REFERENCES games(id) ON DELETE CASCADE,
   developer_id INTEGER REFERENCES developers(id) ON DELETE CASCADE,
   PRIMARY KEY (game_id, developer_id)
